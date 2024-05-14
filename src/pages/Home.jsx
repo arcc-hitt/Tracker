0a2f24c5eec6ca7c751fbc1a61b250cca0053f86
Archit Mahule <mahulearchit@gmail.com>
2024-05-15 02:02:14 +0530

    add graph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Progress from '../components/Progress'
 import Goals from '../components/Goals'
+import Graph from '../components/Graph'
 
 const Home = () => {
   return (
     <div className="pb-[70px]">
       <Progress />
       <Goals />
+      <Graph />
       <Navbar />
     </div>
   )
