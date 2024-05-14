f5158287036ddce90364d49bce4e47e5e5729438
Archit Mahule <mahulearchit@gmail.com>
2024-05-15 02:35:36 +0530

    add progress icon

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -21,6 +21,32 @@ const Progress = () => {
     return (
         <div className='w-full h-auto flex justify-center items-center'>
             <div className='w-[312px] h-[78px] gap-0 rounded-2xl bg-custom-radial flex justify-center items-center'>
+            <div className='w-[54.5px] h-[55.73px] flex justify-center items-center mr-2'>
+                    <div className='relative w-full h-full flex justify-center items-center'>
+                        <img
+                            src="/assets/bullseye.png"
+                            alt="icon"
+                            className='absolute mix-blend-color'
+                            style={{ mixBlendMode: 'multiply' }}
+                        />
+                        <div
+                            className='absolute w-full h-full rounded-full'
+                            style={{ backgroundColor: '#8cc4f8', opacity: 1 }}
+                        />
+                        <div
+                            className='absolute w-[31.28px] h-[30.77px] rounded-full'
+                            style={{ backgroundColor: '#FFFFFF', opacity: 1, boxShadow: '0 6px 10px 6px rgba(14, 119, 217, 0.5)' }}
+                        />
+                        <div
+                            className='absolute w-[20.34px] h-[19.94px] rounded-full'
+                            style={{ backgroundColor: '#8cc4f8', opacity: 1 }}
+                        />
+                        <div
+                            className='absolute w-[11.6px] h-[11.36px] rounded-full'
+                            style={{ backgroundColor: '#FFFFFF', opacity: 1 }}
+                        />
+                    </div>
+                </div>
                 <div className='w-[209.32px] h-auto min-h-[51.37px] gap-3 flex flex-col'>
                     <div className='w-auto h-auto min-h-[26px] min-w-[196px] gap-2 flex flex-col'>
                         <span className='w-[192px] h-[10px] font-dm-sans text-sm font-bold leading-[18.55px] text-left text-[#FFFFFF]'>
